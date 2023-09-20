6d282d9a5fde4444be95bdaacc4e62c537fa6836
DevDominik <71924117+DevDominik@users.noreply.github.com>
2023-09-20 10:08:19 +0200

    Add files via upload

---
+++ WpfApp1/Fuvar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Fuvar
+    {
+        int id, teljesIdotartam;
+        string indulasiIdopont, fizetesiMod;
+        double megtettTavolsag, fizetettOsszeg, borravalo;
+        public Fuvar(string bevitel)
+        {
+            string[] bontott = bevitel.Split(';');
+            this.id = int.Parse(bontott[0]);
+            this.indulasiIdopont = bontott[1];
+            this.teljesIdotartam = int.Parse(bontott[2]);
+            this.megtettTavolsag = double.Parse(bontott[3]);
+            this.fizetettOsszeg = double.Parse(bontott[4]);
+            this.borravalo = double.Parse(bontott[5]);
+            this.fizetesiMod = bontott[6];
+        }
+        public int Azonosito { get { return id; } }
+        public int TeljesIdotartam { get { return teljesIdotartam; } }
+        public string IndulasiIdopont { get {  return indulasiIdopont; } }
+        public string FizetesiMod { get { return fizetesiMod;} }
+
+        public double Borravalo { get { return borravalo; } }
+        public double MegtettTavolsag { get { return megtettTavolsag; } }
+        public double FizetettOsszeg { get {  return fizetettOsszeg;} }
+
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Fuvar> fuvarok = new List<Fuvar>();
+        bool initFinished = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            // << balra: -
+            // >> jobbra: Orosz Zsombor
+            foreach (string adat in File.ReadAllLines("Datas\\fuvar.csv").Skip(1).ToList())
+            {
+                fuvarok.Add(new Fuvar(adat));
+            }
+            List<int> azonositok = new List<int>();
+            foreach (Fuvar fuvar in fuvarok)
+            {
+                if (!azonositok.Contains(fuvar.Azonosito))
+                {
+                    azonositok.Add(fuvar.Azonosito);
+                    cbTaxi.Items.Add($"{fuvar.Azonosito}");
+                }
+            }
+            StreamWriter sw = new StreamWriter("Hibak.txt");
+            sw.WriteLine("taxi_id;indulas;idotartam;tavolsag;viteldij;borravalo;fizetes_modja");
+            foreach (Fuvar fuvar in fuvarok)
+            {
+                if (fuvar.TeljesIdotartam > 0 && fuvar.FizetettOsszeg > 0 && fuvar.MegtettTavolsag == 0)
+                {
+                    sw.WriteLine($"{fuvar.Azonosito};{fuvar.IndulasiIdopont};{fuvar.TeljesIdotartam};{fuvar.FizetettOsszeg};{fuvar.Borravalo};{fuvar.FizetesiMod}");
+                }
+            }
+            sw.Close();
+            initFinished = true;
+        }
+
+        private void btnOsszesFuvar_Click(object sender, RoutedEventArgs e)
+        {
+            lbOsszesFuvar.Content = fuvarok.Count.ToString();
+        }
+
+        private void cbTaxi_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (initFinished)
+            {
+                int osszesFuvar = 0;
+                double bevetel = 0;
+                foreach (Fuvar fuvar in fuvarok)
+                {
+                    if (fuvar.Azonosito == int.Parse(cbTaxi.SelectedItem.ToString()))
+                    {
+                        osszesFuvar += 1;
+                        bevetel += fuvar.FizetettOsszeg + fuvar.Borravalo;
+                    }
+                }
+                lbTaxi.Content = $"${bevetel}, {osszesFuvar} fuvar";
+            }
+        }
+
+        private void btnFizetesiModok_Click(object sender, RoutedEventArgs e)
+        {
+            lsbFizetesiModok.Items.Clear();
+            Dictionary<string, int> fizetesek = new Dictionary<string, int>();
+            foreach (Fuvar fuvar in fuvarok)
+            {
+                fizetesek[fuvar.FizetesiMod] = 0;
+            }
+            foreach (Fuvar fuvar in fuvarok)
+            {
+                fizetesek[fuvar.FizetesiMod] += 1;
+            }
+            foreach (var item in fizetesek)
+            {
+                lsbFizetesiModok.Items.Add($"{item.Key} - {item.Value}");
+            }
+        }
+
+        private void btnOsszesTav_Click(object sender, RoutedEventArgs e)
+        {
+            double osszesMegtettTav = 0;
+            foreach (Fuvar fuvar in fuvarok)
+            {
+                osszesMegtettTav += fuvar.MegtettTavolsag;
+            }
+            lbOsszesTav.Content = $"{Math.Round(osszesMegtettTav*1.6, 2)}km";
+        }
+
+        private void btnLeghosszabbFuvar_Click(object sender, RoutedEventArgs e)
+        {
+            Fuvar leghosszabbFuvar = fuvarok[0];
+            foreach (Fuvar fuvar in fuvarok)
+            {
+                if (fuvar.TeljesIdotartam > leghosszabbFuvar.TeljesIdotartam)
+                {
+                    leghosszabbFuvar = fuvar;
+                }
+            }
+            lsbLeghosszabbFuvar.Items.Add($"Fuvar hossza: {leghosszabbFuvar.TeljesIdotartam} másodperc");
+            lsbLeghosszabbFuvar.Items.Add($"Taxi azonosító: {leghosszabbFuvar.Azonosito}");
+            lsbLeghosszabbFuvar.Items.Add($"Megtett távolság: {Math.Round(leghosszabbFuvar.MegtettTavolsag, 2)}km");
+            lsbLeghosszabbFuvar.Items.Add($"Viteldíj: ${leghosszabbFuvar.FizetettOsszeg}");
+        }
+    }
+}
